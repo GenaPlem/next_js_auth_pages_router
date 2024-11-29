c88acd275822dc9cfd2baff8d2e742cc60b1c6f9
GenaPlem <shainkill1337@gmail.com>
2024-11-29 13:32:59 +0000

    Add redirect from profile page if user unauthenticated

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,24 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession } from "next-auth/react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useRouter } from "next/router";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  // console.log(session);
+  // console.log(status);
+
+  if (status === "loading") {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    router.replace("/auth");
+    return null;
+  }
 
   return (
     <section className={classes.profile}>
