70d74dbc97fbcdbad244428c0418064b1e29b5a8
GenaPlem <shainkill1337@gmail.com>
2024-11-29 13:45:45 +0000

    Add server side logic for redirect

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,24 +1,23 @@
-import { useSession } from "next-auth/react";
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
-import { useRouter } from "next/router";
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const { data: session, status } = useSession();
-  const router = useRouter();
 
-  // console.log(session);
-  // console.log(status);
+  // const { data: session, status } = useSession();
+  // const router = useRouter();
 
-  if (status === "loading") {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // // console.log(session);
+  // // console.log(status);
 
-  if (status === "unauthenticated") {
-    router.replace("/auth");
-    return null;
-  }
+  // if (status === "loading") {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
+
+  // if (status === "unauthenticated") {
+  //   router.replace("/auth");
+  //   return null;
+  // }
 
   return (
     <section className={classes.profile}>
